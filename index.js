3b7ce89eaf1c55f3b8c63430aa9896c912368155
Matt Neary <neary.matt@gmail.com>
2013-03-20 08:08:16 -0400

    server

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var mime = require('mime');
+
+http.createServer(function(req, res) {
+	var path = req.url=='/'?'/index.html':req.url;	
+	fs.stat(__dirname + path, function(err, stat) {
+	    if (!err) {
+	        res.writeHead(200, { 'Content-Type': mime.lookup(__dirname + path) });
+	        fs.createReadStream(__dirname + path).pipe(res);
+	    } else {
+			res.writeHead(404, { 'Content-Type': 'text/html' });
+			res.end("<body>404 Not Found</body>");
+	    }
+	});
+});
